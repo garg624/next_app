3fa2e719cd25edfac13a46f60a3a15ff26b045f1
garg624 <gargayush970@gmail.com>
2023-11-21 19:51:43 +0530

    error in table.tsx is solved

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt='sorry we cannot provide the image'
                       />
                       <p>{invoice.name}</p>
                     </div>
